d9b06727311c73e49bea6e886b050f34186f5f92
lingme <kid--l@hotmail.com>
2020-04-19 18:37:39 +0800

    change listview show windows center position [Fix]

--- RatioConvert.cs
+++ RatioConvert.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Linq;
 
 namespace Gundam.Spike.ScreenInfo
 {
@@ -13,6 +12,27 @@ namespace Gundam.Spike.ScreenInfo
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (double)value / 15f;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MarginConvert : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ObservableCollection<Screen> screenArray)
+            {
+                return new Thickness(
+                    0 - screenArray.Sum(x => x.Bounds.Size.Width)/ 15f ,
+                    0 - screenArray.Sum(x => x.Bounds.Size.Height)/ 15f,
+                    0, 0
+                    );
+            }
+            return new Thickness();
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
