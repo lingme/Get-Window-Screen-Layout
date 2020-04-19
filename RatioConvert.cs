155f6b3b0838128f8590a586d5cb184646f63d89
lingme <kid--l@hotmail.com>
2020-04-19 18:16:02 +0800

    Revert "change listview show windows center position"
    This reverts commit b79bf309750aa992ce5701d4daeb878f40313cc6.

--- RatioConvert.cs
+++ RatioConvert.cs
@@ -12,14 +12,7 @@ namespace Gundam.Spike.ScreenInfo
     {
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is System.Windows.Rect rect)
-            {
-                return (string)parameter == "X" ? rect.Location.X / 15f - rect.Size.Width / 15f / 2 : rect.Location.Y / 15f - rect.Size.Height / 15f / 2;
-            }
-            else
-            {
-                return (double)value / 15f;
-            }
+            return (double)value / 15f;
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
